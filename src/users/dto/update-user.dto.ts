91fd420095abdcd73aa36cf8d5a698f10809c068
Anmol71 <blaze.anmol@rubicotech.in>
2024-01-15 11:00:27 +0530

    remove: partialtype class

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+// import { PartialType } f rom '@nestjs/';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends CreateUserDto {}
