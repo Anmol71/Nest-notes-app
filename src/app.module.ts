33dae4b4e7c5416ed20ea9a78c7e8a101882b366
Anmol71 <blaze.anmol@rubicotech.in>
2024-01-15 19:00:09 +0530

    add: sequelize module in imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './databases/models/user.model';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'blazeanmol',
+      password: 'Rubi@123',
+      database: 'university',
+      models: [User],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
