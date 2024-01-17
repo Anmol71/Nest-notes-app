3cd8de58a79b7fbb6fe6ff09f32038195daa46bd
Anmol71 <blaze.anmol@rubicotech.in>
2024-01-17 10:49:16 +0530

    join: all models in one file

---
+++ src/databases/models/arrayOfModels.ts
@@ -0,0 +1,5 @@
+import { UserModel } from './user.model';
+import { NoteModel } from './note.model';
+import { SharedNoteModel } from './shared-notes.model';
+
+export const models = [UserModel, NoteModel, SharedNoteModel];
