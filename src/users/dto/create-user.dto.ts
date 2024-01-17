365cfb264d957b72beb0b2d283a274b8d467e82f
Anmol71 <blaze.anmol@rubicotech.in>
2024-01-17 11:21:16 +0530

    remove: id field

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -2,10 +2,6 @@ import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
-  @IsInt()
-  id: number;
-
-  @IsNotEmpty()
   @IsString()
   username: string;
 
