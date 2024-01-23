96c97d4f17abda6dcd08d4493c158fc3a995f217
Anmol71 <blaze.anmol@rubicotech.in>
2024-01-23 15:03:23 +0530

    WIP: template login flow

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -17,6 +17,7 @@ import { CreateUserDto } from '../dtos/create-user.dto';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Render('index')
   @Post('register')
   public create(@Body(ValidationPipe) createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
