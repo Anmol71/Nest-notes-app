fd180a29716bb823fe79e114abfab7f3e3466842
Anmol71 <blaze.anmol@rubicotech.in>
2024-01-30 09:53:26 +0530

    add: email column in users table

--- src/databases/migrations/2024.01.29T10.52.27.CreateEmailColumn.ts
+++ src/databases/migrations/2024.01.29T10.52.27.CreateEmailColumn.ts
@@ -1,6 +1,12 @@
+import { DataTypes } from 'sequelize';
 import type { Migration } from '../../umzug';
 
 export const up: Migration = async ({ context: sequelize }) => {
-  await sequelize.getQueryInterface().addColumn('users', {});
+  await sequelize.getQueryInterface().addColumn('users', 'email', {
+    type: DataTypes.STRING,
+    defaultValue: null,
+  });
 };
-export const down: Migration = async ({ context: sequelize }) => {};
+export const down: Migration = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().removeColumn('users', 'email');
+};
