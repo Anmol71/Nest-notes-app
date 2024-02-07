36c2264d0bb09e29c026bf0800991d0da7fb1797
Anmol71 <blaze.anmol@rubicotech.in>
2024-02-07 14:42:51 +0530

    housecleaning: remove unused code

--- src/shared-notes/controllers/shared-notes.controller.ts
+++ src/shared-notes/controllers/shared-notes.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { SharedNotesService } from '../services/shared-notes.service';
 
-// import { UpdateSharedNoteDto } from '../dtos/update-shared-note.dto';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { AuthUser } from 'src/auth/decorators/auth-user.decorator';
 import { SharedNoteModel } from 'src/databases/models/shared-notes.model';
@@ -13,13 +12,6 @@ import { ApiTags } from '@nestjs/swagger';
 @Controller('shared-notes')
 export class SharedNotesController {
   constructor(private readonly sharedNotesService: SharedNotesService) {}
-  // @Post()
-  // create(
-  //   @AuthUser() user: number,
-  //   @Body() createSharedNoteDto: CreateSharedNoteDto,
-  // ) {
-  //   return this.sharedNotesService.create(createSharedNoteDto, user);
-  // }
 
   @Get()
   public async notesSharedToMe(
@@ -27,12 +19,4 @@ export class SharedNotesController {
   ): Promise<SharedNoteModel[]> {
     return this.sharedNotesService.notesSharedToMe(user.id);
   }
-
-  // @Patch(':id')
-  // update(
-  //   @Param('id') id: string,
-  //   @Body() updateSharedNoteDto: UpdateSharedNoteDto,
-  // ) {
-  //   return this.sharedNotesService.update(+id, updateSharedNoteDto);
-  // }
 }
