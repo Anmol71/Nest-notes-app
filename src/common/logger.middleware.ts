b978f46f66c8164634095667ab06a8364eb477de
Anmol71 <blaze.anmol@rubicotech.in>
2024-02-09 15:32:49 +0530

    housecleaning: removed unused code

--- src/common/logger.middleware.ts
+++ src/common/logger.middleware.ts
@@ -6,10 +6,6 @@ export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction): void {
     console.log('Request...', req.body);
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      //   const method = req.body._method;
-      //   console.log(method, 'Method');
-      //   delete req.body._method;
-      //   return method;
       req.method = req.body._method;
     }
     next();
