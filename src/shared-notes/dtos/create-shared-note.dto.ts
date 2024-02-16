ee2f3672c7bb8d121fdc88da6ba20d9c994af075
Anmol71 <blaze.anmol@rubicotech.in>
2024-02-16 12:52:02 +0530

    update: transform property

--- src/shared-notes/dtos/create-shared-note.dto.ts
+++ src/shared-notes/dtos/create-shared-note.dto.ts
@@ -1,12 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform,  } from 'class-transformer';
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateSharedNoteDto {
   @ApiProperty()
   @IsNotEmpty()
-  @IsNumber({allowNaN: false})
-  @Transform(({value}) => parseFloat(value), {toClassOnly: true})
+  @IsNumber({ allowNaN: false })
+  @Transform(({ value }) => parseFloat(value), { toClassOnly: true })
   public shared_with: number;
 }
