2f4e37d8d6b03fa362e56dce126c3cd915fb5378
Anmol71 <blaze.anmol@rubicotech.in>
2024-02-26 18:20:22 +0530

    add: seeder service in provider

--- src/cli-commands/cli-commands.module.ts
+++ src/cli-commands/cli-commands.module.ts
@@ -1,7 +1,8 @@
 import { Logger, Module } from '@nestjs/common';
 import { RouteListService } from './commands/route-list/route-list.service';
+import { SeederService } from './seeder/seeder.service';
 
 @Module({
-  providers: [RouteListService, Logger],
+  providers: [RouteListService, Logger, SeederService],
 })
 export class CliCommandsModule {}
